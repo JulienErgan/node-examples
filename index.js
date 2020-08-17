1342c2a92d991ec1e790d1ff22e77a8c6188125a
Julien Ergan <julien.ergan.code@gmail.com>
2020-08-17 18:53:32 +0900

    Node Callbacks and Error Handling

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-var rect = {
-    perimeter: (x,y) => (2*(x+y)),
-    area: (x,y) => (x*y)
-};
+var rect = require('./rectangle');
+const rectangle = require('./rectangle');
+
+// var rect = {
+//     perimeter: (x,y) => (2*(x+y)),
+//     area: (x,y) => (x*y)
+// };
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + "and b = " + b);
 
-    if (l <= 0 || b <= 0) {
-        console.log("Rectlangle dimensions should be greater than 0: l = " + l + ", and b = " + b);
-    }
-    else {
-        console.log("The area of the rectlangle is: " + rect.area(l, b));
-        console.log("The perimeter of the rectlangle is: " + rect.perimeter(l,b));
-    }
+    rect(l,b,(err,rectangle) => {
+        if (err) {
+            console.log("ERROR", err.message);
+        }
+        else {
+            console.log("The area of the rectangle is: l= " + l + "and b = " + b + "is " + rectangle.area());
+            console.log("The perimeter of the rectangle is: l= " + l + "and b = " + b + "is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement is after the call to the rect()");
+
+    // if (l <= 0 || b <= 0) {
+    //     console.log("Rectangle dimensions should be greater than 0: l = " + l + ", and b = " + b);
+    // }
+    // else {
+    //     console.log("The area of the rectangle is: " + rect.area(l, b));
+    //     console.log("The perimeter of the rectangle is: " + rect.perimeter(l,b));
+    // }
 }
 
 solveRect(2,4);
